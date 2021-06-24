74cf979d895fd9006d581664d5cd303003fc1e12
HaThang <thang.hv.dev@gmail.com>
2021-06-24 05:06:24 -0400

    config source structure

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// These two module are separated form @babel/polyfill which is used for lower babel versions
+import 'core-js/stable';
+import 'core-js/modules/es.symbol';
+import 'regenerator-runtime/runtime';
+
+import { render } from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
